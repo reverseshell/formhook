f1ecb00c17dbad3755e83aefe7abde63c200ac8d
David <revshell11@gmail.com>
2010-11-13 15:33:06 -0800

    Fix htmlentities

--- sample/action.php
+++ sample/action.php
@@ -2,11 +2,11 @@
     echo "Parameters:<br/>";
     foreach (array_keys($_POST) as $key)
     { 
-        print "<b>$key</b> -> " . htmlentities($_POST[$key]) . "<br/>";
+        print "<b>$key</b> -> " . htmlentities($_POST[$key], ENT_QUOTES) . "<br/>";
     }
     
     foreach (array_keys($_GET) as $key)
     {
-        print "<b>$key</b> -> " . htmlentities($_GET[$key]) . "<br/>";
+        print "<b>$key</b> -> " . htmlentities($_GET[$key], ENT_QUOTES) . "<br/>";
     }
 ?>
